eebe7af42c8f89f877bd90a36add2ddbf05886e0
paravatha <prasad.paravatha@gmail.com>
2022-01-10 20:52:08 -0600

    ini

--- setup_env.sh
+++ setup_env.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Python setup
-conda create -n py38-rapids-21.12 -c rapidsai -c nvidia -c conda-forge rapids=21.12 python=3.8 cudatoolkit=11.5 dask-sql
-conda init bash
-conda activate py38-rapids-21.12
+/opt/conda/condabin/conda create -n py38-rapids-21.12 -c rapidsai -c nvidia -c conda-forge rapids=21.12 python=3.8 cudatoolkit=11.5 dask-sql
+/opt/conda/condabin/conda init bash
+/opt/conda/condabin/conda activate py38-rapids-21.12
 pip install ipykernel
-python -m ipykernel install --name=rapids-21.12
+python -m ipykernel install --name=py38-rapids-21.12
 
 # Julia setup
 mkdir julia
