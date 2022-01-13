d1ae9078983471fe7dafc65301877b8fbbc6c588
paravatha <prasad.paravatha@gmail.com>
2022-01-13 17:58:31 -0600

    ini

--- setup_env.sh
+++ setup_env.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Python setup
 /opt/conda/condabin/conda create -n py38-rapids-21.12 -c rapidsai -c nvidia -c conda-forge rapids=21.12 python=3.8 cudatoolkit=11.5 dask-sql
@@ -8,7 +8,7 @@ pip install ipykernel
 python -m ipykernel install --name=py38-rapids-21.12
 
 # Julia setup
-mkdir julia
+mkdir /dli/task/julia
 cd /dli/task/julia
 wget https://julialang-s3.julialang.org/bin/linux/x64/1.7/julia-1.7.1-linux-x86_64.tar.gz
 tar zxvf julia-1.7.1-linux-x86_64.tar.gz
