6a9aabf72757a13f6a81a900aeb0e877343e7828
paravatha <prasad.paravatha@gmail.com>
2022-01-13 17:28:34 -0600

    ini

--- upgrade.sh
+++ upgrade.sh
@@ -5,6 +5,6 @@ su
 apt-get update -y
 apt-get upgrade -y 
 apt-get dist-upgrade -y
-apt-get install update-manager-core
+apt-get install update-manager-core -y
 apt-get install vim -y
 apt-get install cuda -y
